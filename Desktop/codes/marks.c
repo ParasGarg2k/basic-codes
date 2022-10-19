0c4e78d4b731372341969f36d75c85a922d76b52
Paras Garg <parasgarg2k@gmail.com>
2022-10-19 20:51:28 +0530

    added information about codes

---
+++ Desktop/codes/marks.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+//grades of students according to their marks.
+int main(){
+    float marks;
+
+    if(marks>=90) {
+        printf("A \n");
+    }
+
+    else if(marks>=80 && marks <90){
+        printf("B \n");
+    }
+
+    else if(marks>=60 && marks<80)
+{
+    printf("C \n");
+}
+
+   else if (marks>=45 && marks <60){
+    printf("D \n");
+   }
+
+   else{
+    printf("E \n");
+   }
+}
